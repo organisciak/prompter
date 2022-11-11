2c3473fe5b07dfc49fc0479691ce473faf9f0049
Peter Organisciak <organisciak@gmail.com>
2022-11-11 10:34:30 -0700

    Add simple generated image handling

--- prompter/__init__.py
+++ prompter/__init__.py
@@ -1,8 +1,10 @@
-from numpy.random import default_rng
+from numpy.random import default_rng, randint
 import numpy as np
 import pandas as pd
 from pathlib import Path
 import pkg_resources
+import ipywidgets as widgets
+from IPython.display import display
 DATA_PATH = Path(pkg_resources.resource_filename(__name__, 'data'))
 
 rng = default_rng()
@@ -93,3 +95,102 @@ class Prompter():
             ps = PromptSampler(df.prompt.tolist(), weights)
             self.cache[str(path)] = ps
         return self.cache[str(path)]
+
+class ImageOutHandler():
+
+    def __init__(self, outdir=None):
+        '''
+        A class for handling generated images in a notebook. Handles file naming and saving,
+            saving prompts to a file, and having widgets for deleting saved images (or saving unsaved images)
+            on demand.
+
+        If you manually delete images, the prompts.txt file can be analyzed to see which prompt
+            tags were associated with deleted images.
+        '''
+        self.outdir = outdir
+        # save images in memory for when ask=True
+        self.save_queue = {}
+
+    def _clean_prompt(self, prompt):
+        '''
+        Simple cleaning for a filename
+        '''
+        return "".join([x for x in list(prompt.replace(" ", "_")) if x.isalpha() or (x == "_")])
+
+    def reset(self):
+        self.save_queue = {}
+
+    def _save_im_btn(self, b):
+        fname = b.description[5:-1]
+        im = self.save_queue[fname]
+        print("saving", fname)
+        im.save(fname)
+        b.disabled = True
+
+    def _delete_im_btn(self, b):
+        fname = b.description[7:-1]
+        print("deleting", fname)
+        if Path(fname).is_file():
+            os.remove(fname)
+        else:
+            print("Failed to delete")
+        b.disabled = True
+
+    def display_ims(self, images, prompts, fname_suffix=None, unsafe_detected=None, save=True, print_ims=True, 
+                    ask=False, save_prompts=True):
+        '''
+        Handled generated images.
+
+        images: list of PIL images.
+        prompts: prompt string, or list of strings
+        unsafe_detected: safety_checker judgments, if available.
+
+        save: save images to disk (ImageOutHandler.outdir)
+        save_prompts: save image name and prompt to prompts.txt. Only if save=True.
+        fname_suffix: additional identifier. Else a random int is appended.
+        print_ims: show images in notebook. Usually you want this on, unless you're generating a lot at once.
+
+        ask: Buttons for on-demand commands. If save=False, offer a save button. If save=True, offer a delete button.
+        '''
+        # some models don't return the safety checks.
+        if not unsafe_detected:
+            unsafe_detected = [False] * len(images)
+        
+        if type(prompts) == str:
+            prompts = [prompts] * len(images)
+
+        if not save:
+            save_prompts = False
+
+        if not fname_suffix:
+            fname_suffix = str(randint(0, 2147483647))
+
+        for i, (image, unsafe, prompt) in enumerate(zip(images, unsafe_detected, prompts)):
+            if unsafe:
+                print('skipping for content filter')
+                continue
+        
+            clean_prompt = self._clean_prompt(prompt)
+            fname = self.outdir / f"{clean_prompt[:20]}-{fname_suffix}-{i}.png"
+
+            if print_ims:
+                display(image)
+                print(prompt)
+
+            if save:
+                image.save(fname)
+                if ask:
+                    button = widgets.Button(description=f"Delete {fname}?")
+                    display(button)
+                    button.on_click(self._delete_im_btn)
+                    print('-----')
+            elif ask:
+                self.save_queue[str(fname)] = image
+                button = widgets.Button(description=f"Save {fname}?")
+                display(button)
+                button.on_click(self._save_im_btn)
+                print('-----')
+
+            if save_prompts:
+                with open(self.outdir / "prompts.txt", mode='a') as f:
+                    f.write(f"{fname}\t{prompt}\n")
--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='Prompter',
-      version='0.0.2',
+      version='0.1.0',
       description='Text-to-Speech Prompt Helper',
       author='Peter Organisciak',
       author_email='organisciak@gmail.com',
       url='https://www.porganized.com',
-      requires=['pandas', 'numpy'],
+      requires=['pandas', 'numpy', 'ipywidgets', 'numpy'],
       packages=['prompter']
      )
