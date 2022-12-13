889c577581506cfded008a4f66208c92d034fed3
Peter Organisciak <organisciak@gmail.com>
2022-12-12 19:50:03 -0700

    List updates

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
-
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='Prompter',
-      version='0.1.1',
+      version='0.2.1',
       description='Text-to-Speech Prompt Helper',
       author='Peter Organisciak',
       author_email='organisciak@gmail.com',
       url='https://www.porganized.com',
-      requires=['pandas', 'numpy', 'ipywidgets', 'numpy'],
-      packages=['prompter']
+      requires=['pandas', 'smart_open', 'numpy', 'ipywidgets', 'numpy'],
+      packages=['prompter'],
+      include_package_data=True
      )
